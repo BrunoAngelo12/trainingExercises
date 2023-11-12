0a63d5c33e6755872448959a027c34e0a1bf747f
Brn <bruno.trabalhos@outlook.com>
2023-11-12 14:38:29 -0300

    metodos testados, exercicio concluido

--- exercise1/src/exercise4/src/App.java
+++ exercise1/src/exercise4/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Calculadora calc = new Calculadora();
+        double soma = calc.somar(2, 2);
+        System.out.println(soma);
+        double subtracao = calc.subtrair(2, 2);
+        System.out.println(subtracao);
+        double multiplicar = calc.multiplicar(4, 2);
+        System.out.println(multiplicar);
+        double divisao1 = calc.dividir(2, 2);
+        System.out.println(divisao1);
+        double divisao2 = calc.dividir(2, 0);
+        System.out.println(divisao2);
+        double potenciar = calc.potenciar(3, 3);
+        System.out.println(potenciar);
     }
 }
