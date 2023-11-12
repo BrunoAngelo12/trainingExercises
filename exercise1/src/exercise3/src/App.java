55acc7f65c4d5637d12aeafe789060497cd3db17
Brn <bruno.trabalhos@outlook.com>
2023-11-12 14:21:10 -0300

    exercicio concluido

--- exercise1/src/exercise3/src/App.java
+++ exercise1/src/exercise3/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Pessoa pessoa0 = new Pessoa("Bruno", 25, 1.73);
+        pessoa0.apresentar();
     }
 }
