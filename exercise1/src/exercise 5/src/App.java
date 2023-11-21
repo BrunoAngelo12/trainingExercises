e8184bc15e5dae839274dd7469e8fed39c4ed71f
Brn <bruno.trabalhos@outlook.com>
2023-11-20 22:33:04 -0300

    criado metodo de contar vogais

--- exercise1/src/exercise 5/src/App.java
+++ exercise1/src/exercise 5/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ManipuladorStrings manipulador = new ManipuladorStrings();
+        String saudacao = "ola";
+        System.out.println(manipulador.inverter(saudacao));
+        int vogais = manipulador.contarVogais(saudacao);
+        System.out.println(vogais);
+
     }
 }
--- exercise1/src/exercise 5/src/ManipuladorStrings.java
+++ exercise1/src/exercise 5/src/ManipuladorStrings.java
@@ -5,9 +5,20 @@ public class ManipuladorStrings {
 
     public String inverter(String stringOriginal){
         String stringModificada = "";
-        for(int i = stringOriginal.length(); i >= 0; i--){
+        for(int i = stringOriginal.length() - 1; i >= 0; i--){
             stringModificada += stringOriginal.charAt(i);
         }
         return stringModificada;
+    }
+
+    public int contarVogais(String stringOriginal){
+        int stringResultado =0;
+        for(int i = 0; i < stringOriginal.length(); i++){
+            if(stringOriginal.charAt(i) == 'a' || stringOriginal.charAt(i) == 'e' || stringOriginal.charAt(i) == 'i'
+            || stringOriginal.charAt(i) == 'o' || stringOriginal.charAt(i) == 'u'){
+                stringResultado++;
+            }
+        }
+        return stringResultado;
     }
 }
